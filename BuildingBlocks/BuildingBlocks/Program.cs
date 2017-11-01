baa66f3c56e7e45775fb21af6dcc011c002c7cd6
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-11-01 21:45:25 +0000

    Using string, int and var

---
+++ BuildingBlocks/BuildingBlocks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVariable = "Hello World";
+            Console.WriteLine(myVariable);
+
+            int myFirstValue = 23;
+            double mySecondValue = 45.34;
+            // int is being converted to double here
+            double sum = myFirstValue + mySecondValue;
+            Console.WriteLine(sum);
+
+        }
+    }
+}
