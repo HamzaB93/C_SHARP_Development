9534d92ac618d038545df892c1ec02a9a9cae015
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-11-04 19:15:08 +0000

    Basic OOP with object instances and encapsulation

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new warehouse
+            var newWarehouse = new Warehouse(101, "Warehouse 1");
+            // use Findandreturnitem to get back an Item object
+            var myItem = newWarehouse.FindAndReturnItem(101);
+
+            Console.WriteLine("My new item is {0}", myItem.Name);
+        }
+    }
+}
---
+++ OOP/OOP/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Warehouse
+    {
+        public int ID { get; set; }
+        public string WarehouseName { get; set; }
+
+        // Constructor
+        public Warehouse( int ID, string Name)
+        {
+            // Assign the ID to this object
+            this.ID = ID;
+            WarehouseName = Name;
+        }
+
+        // Return an Item object that takes a itemId
+        public Item FindAndReturnItem(int itemId)
+        {
+            // Initialise an Item
+            Item returnItem = new Item() { ID = itemId, Name = "Microsoft Office" };
+            // Return the item
+            return returnItem;
+        }
+    }
+}
