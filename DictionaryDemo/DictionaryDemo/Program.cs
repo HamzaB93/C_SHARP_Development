6b494204f52f1b8b96d5ad214a6ce24902f26c09
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-11-06 21:50:31 +0000

    Use Key Value pair of states and capitals

---
+++ DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Declaring dictionary idenitfy the key and value
+            //Dictionary<string, string> capitals = new Dictionary<string, string>();
+
+            //// Adding to the dictionary first value is key, the second is value
+            //capitals.Add("Alabama", "Mont");
+            //capitals.Add("Alaska", "June");
+            //capitals.Add("Arizona", "Pheonix");
+            //capitals.Add("Massa", "Boston");
+            //capitals.Add("Wyomiing", "chey");
+
+            //// To extract from a dictionary, give the key 
+            //string capitalOfMassa = capitals["Massa"];
+            //Console.WriteLine("Capital of massa is {0} ", capitalOfMassa);
+
+            // Get the dictionary of states were the key is a string and the value is a State
+            var theStates = State.GetStates();
+            // You idenitfy the key and get the STATE object then ask for the captial which is a string
+            //string captitalOfAlaska = theStates["Alaska"].Capital;
+            //Console.WriteLine("THe captical of Alaska is {0}", captitalOfAlaska);
+
+            var theState = theStates["Alaska"];
+            Console.WriteLine("The capital of Alaska is {0}, its population is {1} and its {2} square miles",
+                theState.Capital, theState.Population, theState.Size);
+        }
+    }
+}
---
+++ DictionaryDemo/DictionaryDemo/State.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    public class State
+    {
+        // Properties 
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public int Size { get; set; }
+
+        // Constructtor
+        public State (string capital, int population, int size)
+        {
+            Capital = capital;
+            Population = population;
+            Size = size;
+        }
+
+        // Static method to get the states
+        public static Dictionary<string, State> GetStates()
+        {
+            // Create a dictionary
+            var states = new Dictionary<string, State>();
+            // Create a state object
+            var theState = new State("Mongomery", 123456, 123);
+            //Add that state to the dictionary with a key
+            states.Add("Alabama", theState);
+
+            theState = new State("Juneau", 2468, 246);
+            states.Add("Alaska", theState);
+
+            return states;
+        }
+    }
+}
