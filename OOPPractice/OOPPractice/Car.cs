7e1d0545db94e6feae7b93737f7a8b7df5861e79
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-11-10 21:37:25 +0000

    Make basic car class
    Drives, uses fuel and can be filled

---
+++ OOPPractice/OOPPractice/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Car
+    {
+        ////////////////////// Properties ///////////////////
+
+        public string Manufacturer { get; set; }
+        // Petrol or deisel
+        public string Type { get; set;  }
+        public int Mileage { get; set; }
+        // True is full tank False needs filling
+        public bool TankStatus { get; set; }
+
+        /////////////////  Constructors ///////////////////
+
+        // Default
+        public Car()
+        {
+            Manufacturer = "Unknown";
+            Type = "Petrol";
+            Mileage = 0;
+            TankStatus = true;
+        }
+
+        public Car(string manufacturer, string type, int mileage, bool tankStatus)
+        {
+            Manufacturer = manufacturer;
+            Type = type;
+            Mileage = mileage;
+            TankStatus = tankStatus;
+        }
+
+        //////////////// Methods /////////////////
+
+        // Drive the car to increase the Mileage
+        public void Drive(int milesDriven)
+        {
+            Mileage += milesDriven;
+            TankStatus = false;
+        }
+
+        // If amountOfFule is false, you need to fill up
+        public string CheckTankStatus(bool amountOfFuel)
+        {
+            if (amountOfFuel)
+                return "Your Tank is full.";
+            else
+                return "You need to fill up";
+
+        }
+
+        // Fill the tank and change the boolean to true 
+        public void FillTank()
+        {
+            TankStatus = true;
+        }
+
+    }
+}
---
+++ OOPPractice/OOPPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCar = new Car();
+
+            Console.WriteLine("My cars Mileage is {0}", newCar.Mileage);
+
+            newCar.Drive(100);
+            var tank = newCar.CheckTankStatus(newCar.TankStatus);
+
+            Console.WriteLine("... Now it's {0} and {1}", newCar.Mileage, tank);
+
+            newCar.FillTank();
+            Console.WriteLine(newCar.CheckTankStatus(newCar.TankStatus));
+
+            
+            
+        }
+    }
+}
